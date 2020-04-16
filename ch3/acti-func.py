d185b53d29c747af880b1e4fcf7615d0d7b29667
terfno <sueda.takahito@gmail.com>
2020-04-16 21:21:55 +0900

    :zap::shirt:

--- ch3/acti-func.py
+++ ch3/acti-func.py
@@ -5,9 +5,16 @@ def step_funciton(x): # xはnumpy.array
   y = x > 0 # xに対して不等号で演算すると、arrayの各要素に対して演算の結果がbooleanで入る。yはbooleanの配列
   return y.astype(np.int) # yを型変換→boolからintへ
 
+def sigmoid(x):
+  return 1 / (1 + np.exp(-x)) # numpyのbroadcastによって、np.arrayに対応できている
+
 x = np.arange(-5.0, 5.0, 0.1)
 y = step_funciton(x)
-
 plt.plot(x, y)
+
+x = np.arange(-5.0, 5.0, 0.1)
+y = sigmoid(x)
+plt.plot(x, y)
+
 plt.ylim(-0.1, 1.1) # y座標の範囲を指定
-plt.savefig('step-func.png')
+plt.savefig('acti.png')
