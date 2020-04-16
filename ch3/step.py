add969de1ee8140e4b4164749eb60384ea2d6de9
terfno <sueda.takahito@gmail.com>
2020-04-16 21:15:36 +0900

    :zap: draw step func

--- ch3/step.py
+++ ch3/step.py
@@ -1,8 +1,13 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 def step_funciton(x): # xはnumpy.array
   y = x > 0 # xに対して不等号で演算すると、arrayの各要素に対して演算の結果がbooleanで入る。yはbooleanの配列
   return y.astype(np.int) # yを型変換→boolからintへ
 
-x = np.array([-1.0, 1.0, 2.0])
-print(step_funciton(x))
+x = np.arange(-5.0, 5.0, 0.1)
+y = step_funciton(x)
+
+plt.plot(x, y)
+plt.ylim(-0.1, 1.1) # y座標の範囲を指定
+plt.savefig('step-func.png')
