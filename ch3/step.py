5b642f088a5f50e01ef7431ce8cac05d3a9efeeb
terfno <sueda.takahito@gmail.com>
2020-04-16 21:12:46 +0900

    :tada: update for np.array

--- ch3/step.py
+++ ch3/step.py
@@ -1,5 +1,8 @@
-def step_funciton(x):
-  if x > 0:
-    return 1
-  else:
-    return 0
+import numpy as np
+
+def step_funciton(x): # xはnumpy.array
+  y = x > 0 # xに対して不等号で演算すると、arrayの各要素に対して演算の結果がbooleanで入る。yはbooleanの配列
+  return y.astype(np.int) # yを型変換→boolからintへ
+
+x = np.array([-1.0, 1.0, 2.0])
+print(step_funciton(x))
