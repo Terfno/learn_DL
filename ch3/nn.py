92ba6a4f7865abc1e4a40171cc4970ef1b25accc
terfno <sueda.takahito@gmail.com>
2020-04-18 13:13:07 +0900

    :tada: 3層nnの実装を多次元配列で

--- ch3/nn.py
+++ ch3/nn.py
@@ -6,25 +6,36 @@ def sigmoid(x):
 def identity_function(x):
   return x
 
-X = np.array([1.0, 0.5])
+def init_network():
+  network = {}
 
-# input→第1層
-W1 = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
-B1 = np.array([0.1, 0.2, 0.3])
-A1 = np.dot(X, W1) + B1
+  network['W1'] = np.array([[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]])
+  network['b1'] = np.array([0.1, 0.2, 0.3])
+  network['W2'] = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
+  network['b2'] = np.array([0.1, 0.2])
+  network['W3'] = np.array([[0.1, 0.3], [0.2, 0.4]])
+  network['b3'] = np.array([0.1, 0.2])
 
-Z1 = sigmoid(A1)
+  return network
 
-# 第1層→第2層
-W2 = np.array([[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]])
-B2 = np.array([0.1, 0.2])
-A2 = np.dot(Z1, W2) + B2
+def forward(network, x):
+  W1, W2, W3 = network['W1'], network['W2'], network['W3']
+  b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-Z2 = sigmoid(A2)
+  a1 = np.dot(x, W1) + b1
+  z1 = sigmoid(a1)
+  a2 = np.dot(z1, W2) + b2
+  z2 = sigmoid(a2)
+  a3 = np.dot(z2, W3) + b3
+  y = identity_function(a3)
 
-# 第2層→出力層
-W3 = np.array([[0.1, 0.3], [0.2, 0.4]])
-B3 = np.array([0.1, 0.2])
-A3 = np.dot(Z2, W3) + B3
+  return y
 
-Y = identity_function(A3)
+def main():
+  network = init_network()
+  x = np.array([1.0, 0.5])
+  y = forward(network, x)
+  print(y)
+
+if __name__ == "__main__":
+  main()
