f3d5d1e1a80fe9b3e8ec79d8fabaeffb2f0e43bd
terfno <sueda.takahito@gmail.com>
2020-04-19 19:32:55 +0900

    :zap: PIL導入できた

--- ch3/tegaki.py
+++ ch3/tegaki.py
@@ -1,11 +1,19 @@
 import sys, os
-sys.path.append(os.pardir) # 親ディレクトリのファイルをimportするために
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをimportするために
 
+from PIL import Image
+import numpy as np
 from dataset.mnist import load_mnist
+
+
+def img_show(img):
+  pil_img = Image.fromarray(np.uint8(img))
+  pil_img.save('pil.jpg')
 
 (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
 
-print(x_train.shape)
-print(t_train.shape)
-print(x_test.shape)
-print(t_test.shape)
+img = x_train[0]
+img = img.reshape(28, 28)
+label = t_train[0]
+
+img_show(img)
