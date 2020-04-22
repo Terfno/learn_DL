ba42e950915afa3c8ce80f472b956115e6bd7adb
terfno <sueda.takahito@gmail.com>
2020-04-22 15:09:21 +0900

    :zap: なんかミスってた

--- common.py
+++ common.py
@@ -31,21 +31,21 @@ def cross_entropy_err(y, t):
 
 # gradient
 def numerical_gradient(f, x):
-  h = 1e-4
-  grad = np.zeros_like(x) # xと同じ計上の0配列
+  h = 1e-4 # 0.0001
+  grad = np.zeros_like(x)
 
-  for idx in range(x.size):
+  it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+  while not it.finished:
+    idx = it.multi_index
     tmp_val = x[idx]
-
-    # f(x+h)
     x[idx] = tmp_val + h
-    fxh1 = f(x)
-
-    # f(x-h)
-    x[idx] = tmp_val - h
-    fxh2 = f(x)
-
+    fxh1 = f(x) # f(x+h)
+    
+    x[idx] = tmp_val - h 
+    fxh2 = f(x) # f(x-h)
     grad[idx] = (fxh1 - fxh2) / (2*h)
-    x[idx] = tmp_val # 元の値を代入し直す
-  
+    
+    x[idx] = tmp_val # 値を元に戻す
+    it.iternext()   
+
   return grad
