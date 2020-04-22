c7e556af95862ee117166d6965ed371842c45cd7
terfno <sueda.takahito@gmail.com>
2020-04-22 13:51:46 +0900

    :+1: write gradient

--- ch4/numerical_diff.py
+++ ch4/numerical_diff.py
@@ -5,6 +5,26 @@ def numerical_diff(f, x):
   h = 1e-4
   return (f(x+h) - f(x-h)) / (2*h)
 
+def numerical_gradient(f, x):
+  h = 1e-4
+  grad = np.zeros_like(x) # xと同じ計上の0配列
+
+  for idx in range(x.size):
+    tmp_val = x[idx]
+
+    # f(x+h)
+    x[idx] = tmp_val + h
+    fxh1 = f(x)
+
+    # f(x-h)
+    x[idx] = tmp_val - h
+    fxh2 = f(x)
+
+    grad[idx] = (fxh1 - fxh2) / (2*h)
+    x[idx] = tmp_val # 元の値を代入し直す
+  
+  return grad
+
 def function_1(x):
   return 0.01*x**2 + 0.1*x
 
@@ -19,5 +39,4 @@ plt.ylabel("f(x)")
 plt.plot(x, y)
 plt.savefig("diff.png")
 
-print(numerical_diff(function_1, 5))
-print(numerical_diff(function_1, 10))
+print(numerical_gradient(function_2, np.array([3.0, 4.0])))
