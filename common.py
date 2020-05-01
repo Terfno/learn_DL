cbd34ac4a85e134382cad749b87311967c045ba7
terfno <sueda.takahito@gmail.com>
2020-05-01 10:46:49 +0900

    あああああああああああああああああああああああああああああ

--- common.py
+++ common.py
@@ -6,7 +6,10 @@ def step_funciton(x):
   return y.astype(np.int)
 
 def sigmoid(x):
-  return 1 / (1 + np.exp(-x))
+  sigmoid_range = 34.538776394910684
+  x2 = np.maximum(np.minimum(x, sigmoid_range), -sigmoid_range)
+
+  return 1 / (1 + np.exp(-x2))
 
 def relu(x):
   return np.maximum(0, x)
