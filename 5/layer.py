3fc0118cd44853c4e6d62b4dcd0b30ded73b3312
terfno <sueda.takahito@gmail.com>
2020-05-10 14:31:43 +0900

    :tada: write affine layer

--- 5/layer.py
+++ 5/layer.py
@@ -29,9 +29,30 @@ class Sigmoid:
 
     return out
 
-  def backward(self, dout): # dout:diff value of nn output / return dx
+  def backward(self, dout: np.ndarray) -> np.ndarray: # dout:diff value of nn output / return dx
     dx = dout * (1.0 - self.out) * self.out
 
     return dx
 
+
+class Affine:
+  def __init__(self, W: np.ndarray, b: np.ndarray):
+    self.W = W
+    self.b = b
+    self.x = None
+    self.dW = None
+    self.db = None
+
+  def forward(self, x: np.ndarray) -> np.ndarray: # x:input to NN / return sum of bias and product of input to NN and Weights
+    self.x = x
+    out = np.dot(x, self.W) + self.b
+
+    return out
+
+  def backward(self, dout: np.ndarray) -> np.ndarray: # dout: diff value of nn output / return dx
+    dx = np.dot(dout, self.W.T)
+    self.dW = np.dot(self.x.T)
+    self.db = np.sum(dout, axis=0)
+
+    return dx
 
