:tada: check gradient fix some :bag:

5/layer.py
@@ -51,7 +51,7 @@ class Affine:
 
   def backward(self, dout: np.ndarray) -> np.ndarray: # dout: diff value of nn output / return dx
     dx = np.dot(dout, self.W.T)
-    self.dW = np.dot(self.x.T)
+    self.dW = np.dot(self.x.T, dout)
     self.db = np.sum(dout, axis=0)
 
     return dx

5/twolayernet.py
@@ -15,9 +15,9 @@ class TwoLayerNet:
 
     # generate layer
     self.layers = OrderedDict()
-    self.layers[Affine1] = Affine(self.params['W1'], self.params['b1'])
-    self.layers[Relu1] = Relu()
-    self.layers[Affine2] = Affine(self.params['W2'], self.params['b2'])
+    self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+    self.layers['Relu1'] = Relu()
+    self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
 
     self.lastLayer = SoftmaxWithLoss()
 
@@ -40,6 +40,17 @@ class TwoLayerNet:
     accuracy = np.sum(y == t) / float(x.shape[0])
     return accuracy
 
+  def numerical_gradient(self, x: np.ndarray, t: np.ndarray) -> dict: # calc the gradient of weights / x:input to NN, t:correct label / return dict of grad
+    loss_W = lambda W: self.loss(x, t)
+
+    grads = {}
+    grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+    grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+    grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+    grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+    return grads
+
   def gradient(self, x: np.ndarray, t: np.ndarray) -> dict: # calc the gradient of weights / x:input to NN, t:correct label / return dict of gradient
     # forward
     self.loss(x, t)
@@ -55,9 +66,27 @@ class TwoLayerNet:
 
     # config
     grads = {}
-    grads['W1'] = self.layers['Affine1'].dW
-    grads['b1'] = self.layers['Affine1'].db
-    grads['W2'] = self.layers['Affine2'].dW
-    grads['b2'] = self.layers['Affine2'].db
+    grads['W1'], grads['b1'] = self.layers['Affine1'].dW, self.layers['Affine1'].db
+    grads['W2'], grads['b2'] = self.layers['Affine2'].dW, self.layers['Affine2'].db
 
     return grads
+
+def numerical_gradient(f, x):
+  h = 1e-4 # 0.0001
+  grad = np.zeros_like(x)
+
+  it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+  while not it.finished:
+    idx = it.multi_index
+    tmp_val = x[idx]
+    x[idx] = tmp_val + h
+    fxh1 = f(x) # f(x+h)
+
+    x[idx] = tmp_val - h
+    fxh2 = f(x) # f(x-h)
+    grad[idx] = (fxh1 - fxh2) / (2*h)
+
+    x[idx] = tmp_val # 値を元に戻す
+    it.iternext()
+
+  return grad